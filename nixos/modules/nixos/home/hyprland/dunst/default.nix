7a44d6ebcc832e55f940ac328f389a537fb40d9f
fcalell <frankie.calella@gmail.com>
2024-03-19 10:18:12 +0100

    update dunst style

--- nixos/modules/nixos/home/hyprland/dunst/default.nix
+++ nixos/modules/nixos/home/hyprland/dunst/default.nix
@@ -1,24 +1,29 @@
 _: {
   services.dunst = {
     enable = true;
-    configFile = ''
-      [global]
-      font = "DroidSansMono Nerd Font 12"
-      frame_color = "#89B4FA"
-      separator_color= frame
-
-      [urgency_low]
-      background = "#1E1E2E"
-      foreground = "#CDD6F4"
-
-      [urgency_normal]
-      background = "#1E1E2E"
-      foreground = "#CDD6F4"
-
-      [urgency_critical]
-      background = "#1E1E2E"
-      foreground = "#CDD6F4"
-      frame_color = "#FAB387"
-    '';
+    settings = {
+      global = {
+        font = "DroidSansMono Nerd Font 12";
+        frame_color = "#89B4FA";
+        separator_color = "frame";
+        icon_position = "left";
+        corner_radius = 8;
+        text_icon_padding = 8;
+        frame_width = 1;
+      };
+      urgency_low = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+      };
+      urgency_normal = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+      };
+      urgency_critical = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+        frame_color = "#FAB387";
+      };
+    };
   };
 }

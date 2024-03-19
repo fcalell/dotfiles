eea9dbbf26d96431786faffa818cbd154713dcec
fcalell <frankie.calella@gmail.com>
2024-03-19 11:50:54 +0100

    add android

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,6 +15,10 @@
       url = "github:Mic92/nix-ld";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, hyprland, home-manager, ... }@inputs: {
--- nixos/modules/common/home/default.nix
+++ nixos/modules/common/home/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./terminal ./zsh ./neovim ./cli-tools ./fonts ./env ];
+  imports = [ ./android ./terminal ./zsh ./neovim ./cli-tools ./fonts ./env ];
   home.packages = [ pkgs.google-chrome ];
 }

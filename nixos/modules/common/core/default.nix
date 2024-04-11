37dbae113880e388eb3f49ace1d64947fcab2f30
Francesco Calella <francesco.calella@vodafone.com>
2024-04-11 18:34:27 +0200

    not using auto gc

--- nixos/modules/common/core/default.nix
+++ nixos/modules/common/core/default.nix
@@ -10,11 +10,6 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
   };
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];

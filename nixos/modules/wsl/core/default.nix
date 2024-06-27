035000b85de848a6f2a6648bc05c5384029efa40
Francesco Calella <frankie.calella@gmail.com>
2024-06-27 11:26:14 +0200

    add wsl

--- nixos/modules/wsl/core/default.nix
+++ nixos/modules/wsl/core/default.nix
@@ -1,12 +1,10 @@
-{inputs,...}:{
-  imports = [
-    inputs.nixos-wsl.nixosModules.wsl
-    ./user.nix
-  ];
+{ inputs, ... }: {
+  imports = [ inputs.nixos-wsl.nixosModules.wsl ./user.nix ];
 
   wsl.enable = true;
   wsl.defaultUser = "fcalell";
+  wsl.useWindowsDriver = true;
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
-  
+  programs.dconf.enable = true;
 }

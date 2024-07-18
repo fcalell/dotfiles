d46432ce12d6f481a5ff735f461ee8702609c142
fcalell <frankie.calella@gmail.com>
2024-07-18 10:11:29 +0200

    add vlc

--- nixos/modules/nixos/home/default.nix
+++ nixos/modules/nixos/home/default.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }: {
   imports = [ ./gtk ./hyprland ./streamrip ];
-  home.packages = with pkgs; [ google-chrome xfce.thunar onlyoffice-bin ];
+  home.packages = with pkgs; [ google-chrome xfce.thunar onlyoffice-bin vlc ];
 }

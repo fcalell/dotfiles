41ab2315ef52f560d20fb29a749ed84a3cb17b18
fcalell <frankie.calella@gmail.com>
2024-09-06 11:47:55 +0200

    update ld

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,8 +15,9 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-ld = {
-      url = "github:Mic92/nix-ld";
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
--- nixos/modules/nixos/core/nix-ld.nix
+++ nixos/modules/nixos/core/nix-ld.nix
@@ -1,4 +1,28 @@
-{ inputs, ... }: {
-  imports = [ inputs.nix-ld.nixosModules.nix-ld ];
-  programs.nix-ld.dev.enable = true;
+{ inputs, pkgs, ... }: {
+  imports = [ inputs.nix-index-database.nixosModules.nix-index ];
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = (with pkgs; [
+    electron
+    glib
+    nss_latest
+    nspr
+    dbus
+    at-spi2-atk
+    cups
+    libdrm
+    gtk3
+    pango
+    cairo
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libxcb
+    mesa
+    expat
+    libxkbcommon
+    alsa-lib
+  ]);
 }

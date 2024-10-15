d6a547e9fba454905bea47eccf15ec567cbcccf2
fcalell <frankie.calella@gmail.com>
2024-10-15 15:23:16 +0200

    remove rip config

--- nixos/modules/nixos/home/streamrip/default.nix
+++ nixos/modules/nixos/home/streamrip/default.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }: {
   home.packages = with pkgs; [ streamrip ];
-  xdg.configFile."streamrip/config.toml" = {
-    source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/nixos/modules/nixos/home/streamrip/config.toml";
-  };
+  # xdg.configFile."streamrip/config.toml" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink
+  #     "${config.home.homeDirectory}/nixos/modules/nixos/home/streamrip/config.toml";
+  # };
 }

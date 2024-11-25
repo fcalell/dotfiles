d1f4f7416a2c6045613ca01a3623bd7ff1435586
fcalell <frankie.calella@gmail.com>
2024-11-25 10:11:58 +0100

    add nx nvim plugin

--- nixos/modules/common/home/neovim/lua/plugins/telescope.lua
+++ nixos/modules/common/home/neovim/lua/plugins/telescope.lua
@@ -103,6 +103,23 @@ return {
 		end,
 	},
 	{
+		"Equilibris/nx.nvim",
+
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+
+		opts = {
+			-- See below for config options
+			nx_cmd_root = "npx nx",
+		},
+
+		-- Plugin will load when you use these keys
+		keys = {
+			{ "<leader>px", "<cmd>Telescope nx actions<CR>", desc = "nx actions" },
+		},
+	},
+	{
 		"windwp/nvim-spectre",
 		lazy = true,
 		keys = function()

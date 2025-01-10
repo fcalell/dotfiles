be692aa5efedcc09b961720a0eb286668b4a5c00
fcalell <frankie.calella@gmail.com>
2025-01-10 11:10:54 +0100

    add system image

--- nixos/modules/nixos/home/android/default.nix
+++ nixos/modules/nixos/home/android/default.nix
@@ -15,6 +15,7 @@
       platforms-android-35
       sources-android-35
       system-images-android-35-default-x86-64
+      system-images-android-35-google-apis-playstore-x86-64
       ndk-26-1-10909125
       skiaparser-3
       sources-android-34

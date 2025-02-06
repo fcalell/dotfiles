1edb7de87b69d834471b6fa61ae4c50028beae7c
fcalell <frankie.calella@gmail.com>
2025-02-07 00:54:48 +0100

    update

--- nixos/modules/nixos/home/hyprland/waybar/default.nix
+++ nixos/modules/nixos/home/hyprland/waybar/default.nix
@@ -4,9 +4,13 @@
     enable = true;
     systemd.enable = true;
   };
-  xdg.configFile.waybar = {
+  xdg.configFile."waybar/config.jsonc" = {
     source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/nixos/modules/nixos/home/hyprland/waybar/config";
+      "${config.home.homeDirectory}/nixos/modules/nixos/home/hyprland/waybar/config/config.jsonc";
   };
 
+  xdg.configFile."waybar/style.css" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/nixos/modules/nixos/home/hyprland/waybar/config/style.css";
+  };
 }

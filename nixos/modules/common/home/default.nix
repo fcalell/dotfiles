644049d11e701590e69290dad1f657fb99890c73
fcalell <frankie.calella@gmail.com>
2025-05-26 17:16:41 +0200

    remove torrent

--- nixos/modules/common/home/default.nix
+++ nixos/modules/common/home/default.nix
@@ -1,11 +1,4 @@
 { pkgs, ... }: {
   imports = [ ./terminal ./zsh ./neovim ./cli-tools ./env ./theme ./android ];
-  home.packages = with pkgs; [
-    sqlite-web
-    qbittorrent
-    inkscape
-    gimp
-    streamrip
-    code-cursor
-  ];
+  home.packages = with pkgs; [ sqlite-web inkscape gimp streamrip code-cursor ];
 }
